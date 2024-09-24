76ad43d115f5e5469a5f31daf03d906e16bc5008
rendizi <baglanov.a0930@gmail.com>
2024-09-24 08:31:09 +0500

    new origin

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://10.10.65.229:3000', 'http://localhost:3000'], 
+    origin: ['http://10.10.65.229:3000', 'http://localhost:3000','http://10.10.65.182:3000'], 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   })
